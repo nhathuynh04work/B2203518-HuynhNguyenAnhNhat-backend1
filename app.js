79fe096cb2f41f618c7df4c616b9918cb49db131
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-25 10:35:36 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+/* eslint-disable no-unused-vars */
 import express from "express";
 import cors from "cors";
+import contactsRouter from "./app/routes/contact.route.js";
+import { ApiError } from "./app/api-error.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.json({ message: "Welcome to contact book application" });
+// main routes
+app.use("/api/contacts", contactsRouter);
+
+// not found route
+app.use((req, res, next) => {
+	throw new ApiError(404, "Resource not found");
+});
+
+// central error handler
+app.use((error, req, res, next) => {
+	res.status(error.statusCode || 500).json({
+		message: error.message || "Internal Server Error",
+	});
 });
 
 export default app;
